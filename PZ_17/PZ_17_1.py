679466dff86a97d70a874a2fd8b0beb40060669f
xesik <7olvi17@mail.ru>
2023-05-22 22:56:16 +0300

    pz-17

--- PZ_17/PZ_17_1.py
+++ PZ_17/PZ_17_1.py
@@ -1,16 +1,18 @@
-# Создайте класс «Счетчик», который имеет атрибут текущего значения и методы для
-# инкремента и декремента значения.
-class Chet:  # создание класса
-    def __init__(self, a):
-        self.a = a
+# -*- coding: utf-8 -*-
+# РЎРѕР·РґР°Р№С‚Рµ РєР»Р°СЃСЃ В«РЎС‡РµС‚С‡РёРєВ», РєРѕС‚РѕСЂС‹Р№ РёРјРµРµС‚ Р°С‚СЂРёР±СѓС‚ С‚РµРєСѓС‰РµРіРѕ Р·РЅР°С‡РµРЅРёСЏ Рё РјРµС‚РѕРґС‹ РґР»СЏ
+# РёРЅРєСЂРµРјРµРЅС‚Р° Рё РґРµРєСЂРµРјРµРЅС‚Р° Р·РЅР°С‡РµРЅРёСЏ.
+class Chet:  # СЃРѕР·РґР°РЅРёРµ РєР»Р°СЃСЃР°
+    def __init__(self, balance):
+        self.balance = balance
 
     def plus(self, y=0):
-        return self.a + y
+        return self.balance + y
 
     def minus(self, x=0):
-        return self.a - x
+        return self.balance - x
 
 
 primer = Chet(1)
+print(primer.__dict__)
 print(primer.plus(100))
 print(primer.minus(100))
